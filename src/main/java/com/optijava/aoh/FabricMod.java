5d3d3f5d34f3ccf12b9d96bd7ad4bdb71a78cfb9
OptiJava <OptiJava@protonmail.com>
2022-07-16 19:50:51 +0800

    change mod setting

---
+++ src/main/java/com/optijava/aoh/FabricMod.java
@@ -0,0 +1,10 @@
+package com.optijava.aoh;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
